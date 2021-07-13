0435e56aba2d6971ad996926d14a603c6518b593
nomagno <alvarosaez316@gmail.com>
2021-07-13 14:51:46 +0200

    Finish initial parser implementation

--- src/tracks.c
+++ src/tracks.c
@@ -20,16 +20,16 @@ int get_track(char *track, struct Bezier track_shape[]) {
 
   FILE *fp;
 
-  long unsigned i = 0, x;
+  long unsigned i = 0, x, curvetrack = 0, sectiontrack = 0, valuetrack = 0;
   int len = sizeof(struct Bezier);
-  int index[40], width[40], test1, test2, test3, test4, test5, test_full;
+  int index[40], width[40], test_full = 0;
   for (x = 0; x < 10; x++) {
     strcpy(section[x], "");
     strcpy(param[x], "");
     strcpy(value[x], "");
   }
-  for (x = 0; x < 10; x++) {
-	strcpy(buffer[x], "");
+  for (x = 0; x < 80; x++) {
+    strcpy(buffer[x], "");
   }
 
   fp = fopen(track, "r");
@@ -38,35 +38,48 @@ int get_track(char *track, struct Bezier track_shape[]) {
     perror("No such file or directory");
   }
 
-  for(x = 0; x < 80 && (fgets(buffer[x], sizeof(buffer[x]), fp) != NULL); x++){
-  	sscanf(buffer[x], "[%[^]]]", section[0]);
-	sscanf(buffer[x], " %5s - %5s", param[0], value[0]);
-	sscanf(buffer[x], "[%[^]]]", section[1]);
-		
-	scanf(buffer[x], " (%1i) (%f, %f) (%f, %f) (%f, %f) (%1i) ", &index[i],
-	&curves[0].p1.x, &curves[0].p1.y, &curves[0].p2.x,
-	&curves[0].p2.y, &curves[0].p3.x, &curves[0].p3.y, &width[i]);
+  for (x = 0; x < 80 && (fgets(buffer[x], sizeof(buffer[x]), fp) != NULL);
+       x++) {
+    if (sscanf(buffer[x], " [%[^]]] ", section[sectiontrack]) != 1) {
+      test_full += 1;
+    } else {
+      sectiontrack++;
+    }
 
-	sscanf(buffer[x], " (%1i) (%f, %f) (%f, %f) (%f, %f) (%1i) + %s = %s ",
-                 &index[i], &curves[0].p1.x, &curves[0].p1.y, &curves[0].p2.x,
-                 &curves[0].p2.y, &curves[0].p3.x, &curves[0].p3.y, &width[i],
-                 param[1], value[1]);
+    if (sscanf(buffer[x], " %s - %s ", param[valuetrack], value[valuetrack]) !=
+        2) {
+      test_full += 1;
+    } else {
+      valuetrack++;
+    }
+
+    if (sscanf(buffer[x], " (%1i) (%f, %f) (%f, %f) (%f, %f) (%1i) ",
+               &index[curvetrack], &curves[curvetrack].p1.x,
+               &curves[curvetrack].p1.y, &curves[curvetrack].p2.x,
+               &curves[curvetrack].p2.y, &curves[curvetrack].p3.x,
+               &curves[curvetrack].p3.y, &width[curvetrack]) != 8) {
+      if (sscanf(buffer[x],
+                 " (%1i) (%f, %f) (%f, %f) (%f, %f) (%1i) + %s = %s ",
+                 &index[curvetrack], &curves[curvetrack].p1.x,
+                 &curves[curvetrack].p1.y, &curves[curvetrack].p2.x,
+                 &curves[curvetrack].p2.y, &curves[curvetrack].p3.x,
+                 &curves[curvetrack].p3.y, &width[curvetrack],
+                 param[valuetrack], value[valuetrack]) != 10) {
+        test_full += 1;
+      } else if (curvetrack < 40) {
+        curvetrack++;
+        valuetrack++;
+      }
+    }
+
+    else if (curvetrack < 40) {
+      curvetrack++;
+    }
   }
 
-  
-  test3 = fscanf(fp, " [ %8s ] ", section[1]);
-
-  test4 = fscanf(fp, " (%1i) (%f, %f) (%f, %f) (%f, %f) (%1i) ", &index[i],
-                 &curves[0].p1.x, &curves[0].p1.y, &curves[0].p2.x,
-                 &curves[0].p2.y, &curves[0].p3.x, &curves[0].p3.y, &width[i]);
-
-  test5 = fscanf(fp, " (%1i) (%f, %f) (%f, %f) (%f, %f) (%1i) + %s = %s ",
-                 &index[i], &curves[0].p1.x, &curves[0].p1.y, &curves[0].p2.x,
-                 &curves[0].p2.y, &curves[0].p3.x, &curves[0].p3.y, &width[i],
-                 param[1], value[1]);
   for (i = 0; i < (sizeof(curves) / len); i++) {
     track_shape[i] = curves[i];
   };
- // test_full = test1 + test2 + test3 + test4 + test5;
+  // test_full = test1 + test2 + test3 + test4 + test5;
   return (0);
 }
