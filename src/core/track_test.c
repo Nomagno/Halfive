fe5272de3edaf7a1e9f919453078431a75efc8c4
Nomagno <alvarosaez316@gmail.com>
2021-07-24 19:32:56 +0200

    Start with standarization of both the core library and the base rendering code

--- src/core/track_test.c
+++ src/core/track_test.c
@@ -2,18 +2,18 @@
 
 int main() {
   int y;
-  struct Bezier track_storage[40];
   char full_path[40], *path;
   printf("What's the name of the track file you'd like to test-parse?\n");
   scanf("%s", path);
   strcpy(full_path, "../../assets/tracks/");
   strcat(full_path, path);
-  int result = get_track(full_path, track_storage);
+  struct Track test_track = get_track(full_path);
   for (y = 0; y < 40; y++) {
-    printf("(%i, %i) (%i, %i) (%i, %i)\n", track_storage[y].p1.x,
-           track_storage[y].p1.y, track_storage[y].p2.x, track_storage[y].p2.y,
-           track_storage[y].p3.x, track_storage[y].p3.y);
+  
+    printf("(%i, %i) (%i, %i) (%i, %i)\n", test_track.segments[y].shape.p1.x,
+           test_track.segments[y].shape.p1.y, test_track.segments[y].shape.p2.x, test_track.segments[y].shape.p2.y,
+           test_track.segments[y].shape.p3.x, test_track.segments[y].shape.p3.y);
   }
-  printf("%d\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n", result, section[0], param[0], value[0],
+  printf("%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n", "SHOW ME YOUR MOVES!", section[0], param[0], value[0],
          section[1], param[1], value[1], param[2], value[2], param[3], value[3], param[4], value[4]);
 }
--- src/hwt-gui/render_main.c
+++ src/hwt-gui/render_main.c
@@ -1,7 +1,90 @@
+#include "../core/tracks.c"
+#include "input_main.c"
 #include <SDL2/SDL.h>
-#include<SDL2/SDL_image.h>
-#include<SDL2/SDL_ttf.h>
-#include<SDL2/SDL_mixer.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_mixer.h>
+#include <SDL2/SDL_ttf.h>
+#include <stdbool.h>
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+int i;
 
-void render(){
+SDL_Window *sdlWindow = NULL;
+SDL_Renderer *sdlRenderer = NULL;
+
+void sdlExit();
+void sdlError();
+
+bool sdlLoadTexture(int sprite_num, char paths[sprite_num][100],
+                    SDL_Texture *returnArray[sprite_num]) {
+
+  bool any_errors = false;
+  SDL_Texture *arrayTexture[sprite_num];
+  for (i = 0; i < sprite_num; i++) {
+    arrayTexture[i] = NULL; 
+    }
+
+  SDL_Surface *loadedSurfaces[sprite_num];
+
+  for (i = 0; i < sprite_num; i++) {
+    loadedSurfaces[i] = IMG_Load(paths[i]);
+    if (loadedSurfaces[i] == NULL) {
+      sdlError();
+      any_errors = true;
+    }
+    arrayTexture[i] =
+        SDL_CreateTextureFromSurface(sdlRenderer, loadedSurfaces[i]);
+    if (arrayTexture[i] == NULL) {
+      sdlError();
+      any_errors = true;
+    }
+    SDL_FreeSurface(loadedSurfaces[i]);
+  }
+
+  for (i = 0; i < sprite_num; i++) {
+    returnArray[i] = arrayTexture[i];
+  }
+
+  return any_errors;
+}
+bool sdlLoadMedia() {}
+
+bool sdlInit() {
+  bool success = true;
+
+  sdlWindow = SDL_CreateWindow("Half-World Track Editor",
+                               SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+                               SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+  if (sdlWindow == NULL) {
+    sdlError();
+    success = false;
+  }
+
+  SDL_SetRenderDrawColor(sdlRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
+
+  int imgInit = IMG_INIT_PNG;
+  if (!(IMG_Init(imgInit) & imgInit)) {
+    sdlError();
+    success = false;
+  }
+  return success;
+}
+
+bool loadMedia() {
+
+}
+
+void sdlRender() {
+  bool should_quit;
+  bool could_init = sdlInit();
+  SDL_Event event1;
+
+  while (!should_quit) {
+
+    while (SDL_PollEvent(&event1) != 0) {
+      if (event1.type == SDL_QUIT) {
+        should_quit = true;
+      }
+    }
+  }
 }
