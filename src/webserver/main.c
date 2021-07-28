60a3df1367e28f8e62e4250db23839856438143c
Nomagno <alvarosaez316@gmail.com>
2021-07-28 22:35:27 +0200

    Make POSIX-Compliant systems use Named Pipes

--- src/webserver/main.c
+++ src/webserver/main.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
+
+#if defined(__unix__) || defined (__APPLE__)
+  #include <sys/stat.h>
+#endif
+
 #include "WebServer/include/ws.h"
 
 FILE *input_file;
@@ -18,14 +23,19 @@ void onclose(int fd)
 void onmessage(int fd, const unsigned char *msg, uint64_t size, int type)
 {
 
-    printf("%s", msg);
+    printf("%s\n", msg);
     fseek(input_file, 0, SEEK_SET);
     fprintf(input_file, "%s\n", msg);
 }
 
 int main()
 {
+    #if defined(__unix__) || defined (__APPLE__)
+    mkfifo("input.pipe", 0666);
+    input_file = fopen("input.pipe", "w");
+    #else
     input_file = fopen("input.txt", "w");
+    #endif
     struct ws_events evs;
     evs.onopen    = &onopen;
     evs.onclose   = &onclose;
