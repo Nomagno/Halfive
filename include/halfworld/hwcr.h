478b5460e642ce7a5a8d98455ec6864dfe71d3e8
Nomagno <nomagno@noreply.gitea.io>
2022-07-08 19:12:29 +0800

    Update 'include/halfworld/hwcr.h'

--- include/halfworld/hwcr.h
+++ include/halfworld/hwcr.h
@@ -2,10 +2,12 @@
 #define HWCOROUTE_H
 
 #define HWCR_YIELD(n) frame->pc +=1; return n;
-#define HWCR_RETURN(n) *frame = (HWCR_Frame){0}; return n;
+#define HWCR_RETURN(n) frame->pc = 0; HWCR_Reset(frame->data) return n;
 
 typedef struct {
 	unsigned pc;
 	void *data;
 } HWCR_Frame;
+
+void HWCR_Reset(void *data);
 #endif
