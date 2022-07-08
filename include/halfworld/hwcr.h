ac0bd55ddf49db924982f9804a7df13bb392febf
Nomagno <nomagno@noreply.gitea.io>
2022-07-08 19:15:29 +0800

    Add missing semicolon

--- include/halfworld/hwcr.h
+++ include/halfworld/hwcr.h
@@ -2,7 +2,7 @@
 #define HWCOROUTE_H
 
 #define HWCR_YIELD(n) frame->pc +=1; return n;
-#define HWCR_RETURN(n) frame->pc = 0; HWCR_Reset(frame->data) return n;
+#define HWCR_RETURN(n) frame->pc = 0; HWCR_Reset(frame->data); return n;
 
 typedef struct {
 	unsigned pc;
