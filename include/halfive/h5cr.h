76b638acdbdd611a1bc57423af3b7b2e8b513cbe
Nomagno <alvarosaez316@gmail.com>
2023-01-08 14:12:42 +0100

    Improve h5cr.h

--- include/halfive/h5cr.h
+++ include/halfive/h5cr.h
@@ -3,22 +3,26 @@
 #ifndef H5COROUTE_H
 #define H5COROUTE_H
 
-#define H5CR_YIELD(n)                                                          \
-    do {                                                                       \
-	frame->pc += 1;                                                        \
-	return n;                                                              \
-    } while (0)
-#define H5CR_RETURN(n)                                                         \
-    do {                                                                       \
-	frame->pc = 0;                                                         \
-	H5CR_Reset(frame->data);                                               \
-	return n;                                                              \
-    } while (0)
+#define H5CR_START()     \
+    if (frame->pc == 0){
+
+#define H5CR_YIELD(n)    \
+	frame->pc = __LINE__;\
+	return n;            \
+	} else if (frame->pc >= __LINE__){
+
+#define H5CR_RETURN(n, resetter)\
+	frame->pc = 0;              \
+	resetter(frame->data);      \
+	return n;
+
+#define H5CR_END() \
+	}
+
 
 typedef struct {
     unsigned pc;
     void *data;
 } H5CR_Frame;
 
-void H5CR_reset(void *data);
 #endif
