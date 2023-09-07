efc6ac42444816f81e9bf3fb6f733da8cb9d7ce7
Nomagno <alvarosaez316@gmail.com>
2023-09-07 02:32:28 +0200

    Add note for future usage

--- docs/design/genhtml_mod.sh
+++ docs/design/genhtml_mod.sh
@@ -7,5 +7,9 @@ rm -rf html/
 mkdir html
 for i in $(ls md/*.md); do
 	pandoc --ascii -f markdown-auto_identifiers -t html $i |
+# FOR ../GENHTML.SH, MEANS FOR THE MAIN WEBSITE'S GENERATION NEEDS AND MAINLY SPECS I GUESS, INSTEAD OF THIS LINE:
 	cat "css_""$(rm_ext $i)" - > "html/""$(rm_ext $i)"".html"
+# USE THIS:
+#   cat "$1/""css_""$(rm_ext $i)" - > "$1/""$(rm_ext $i)"".html"
+# PLEASE REREAD THIS AND THINK ABOUT IT BEFORE APPLYING BLINDLY, I DO NOT REMEMBER THE RATIONALE
 done
